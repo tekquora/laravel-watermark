46db16d7e8bb3b3ef2e5bec285a840d0936ea38d
Luqman-dev-pro <jmluqman77@gmail.com>
2025-12-17 20:18:21 +0530

    Refactor layout handling in wrapper view to support component-based and classic Blade layouts

--- resources/views/layouts/wrapper.blade.php
+++ resources/views/layouts/wrapper.blade.php
@@ -1,9 +1,25 @@
-@if (View::exists(config('watermark.views.layout')))
-    @extends(config('watermark.views.layout'))
-@endif
+@php
+    $layout = config('watermark.views.layout');
+@endphp
 
-@if (isset($slot))
-    {{ $slot }}
+@if ($layout === 'layouts.app' && View::exists('components.app-layout'))
+    {{-- Component-based layout (Breeze / Jetstream) --}}
+    <x-app-layout>
+        <div class="py-6">
+            {{ $slot ?? '' }}
+            @yield('content')
+        </div>
+    </x-app-layout>
+@elseif (View::exists($layout))
+    {{-- Classic Blade layout --}}
+    @extends($layout)
+
+    @section('content')
+        {{ $slot ?? '' }}
+        @yield('content')
+    @endsection
 @else
+    {{-- Fallback --}}
+    {{ $slot ?? '' }}
     @yield('content')
 @endif
